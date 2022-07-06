3492e14df0d76872d6103f45fa6367ea60b95503
huouvcti <huouvcti@gmail.com>
2022-07-06 14:35:41 +0900

    no message

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@
 
 const app = require('../app');
 const http = require('http');
-const port = process.env.S_PORT || 3000;
+const port = process.env.S_PORT || 8000;
 const server = http.createServer(app);
 server.listen(port, () => {
     console.log('Server on ' + port);
