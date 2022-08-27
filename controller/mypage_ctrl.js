b52de5942259540543c2b28062fa87985aa8ab7e
ddong3301 <ssuny303@gmail.com>
2022-08-28 00:32:00 +0900

    server

--- controller/mypage_ctrl.js
+++ controller/mypage_ctrl.js
@@ -11,17 +11,16 @@ async function first_login_userInfo(req, res, next) {
         const mbti = req.body.mbti;
         const intro = req.body.intro;
 
-        if (sex == "여자") sex = 1
-        else if (sex == "남자") sex = 2
+        if (sex == "여") sex = 1
+        else if (sex == "남") sex = 2
 
         const parameter = { user_key, nickname, sex, tags, mbti, intro };
-        const db_data = await mypageDAO.first_login_getInfo(parameter);
+        console.log(parameter)
+		await mypageDAO.first_login_getInfo(parameter);
 
-        res.json({
-            "db_data": db_data
-        })
+        res.send({result: "success"})
     } catch (err) {
-        res.send("사용자 정보를 가져올 수 없음")
+        res.send(err)
     }
 }
 
@@ -34,12 +33,12 @@ async function profile_modify(req, res, next) {
         const intro = req.body.intro;
         const tags = req.body.tags;
 
-        if (sex == "여자") sex = 1
-        else if (sex == "남자") sex = 2
+        if (sex == "여") sex = 1
+        else if (sex == "남") sex = 2
         
         const user_parameter = { user_key, nickname };
         let db_data = await mypageDAO.user_profile_modify(user_parameter);
-        const detail_parameter = { sex, mbti, tags, intro };
+        const detail_parameter = {user_key, sex, mbti, tags, intro };
         db_data = await mypageDAO.user_detail_profile_modify(detail_parameter);
 
         res.send({ result: "success" });
