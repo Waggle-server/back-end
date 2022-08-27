57b893c7033898be406f58d7af80775683e3c67f
huouvcti <huouvcti@gmail.com>
2022-08-28 00:39:45 +0900

    Merge branch 'nagyeong' of https://github.com/Waggle-server/back-end into nagyeong
    # Conflicts:
    #	model/mypageDAO.js

--- model/mypageDAO.js
+++ model/mypageDAO.js
@@ -6,12 +6,10 @@ function first_login_getInfo(parameter) {
     return new Promise((resolve, reject) => {
         const queryData = `INSERT INTO user_detail(user_key, intro, sex, mbti, tag) values (?, ?, ?, ?, ?)`;
         db.query(queryData, [parameter.user_key, parameter.intro, parameter.sex, parameter.mbti, parameter.tags], (err, db_data) => {
-            // if(db_data) resolve(db_data);
-            // else reject(err);
-            if(err){
-                reject(err)
-            } else{
-                resolve(db_data)
+            if(err) {
+                reject(err);
+            } else {
+                resolve(db_data);
             }
         })
     })
