85bbc140112677b7e43ff72ad2430548cf584e3f
huouvcti <huouvcti@gmail.com>
2022-08-28 00:33:39 +0900

    no message

--- routes/mypageRouter.js
+++ routes/mypageRouter.js
@@ -8,6 +8,8 @@ const mypage_ctrl = require("../controller/mypage_ctrl");
 router.post("/first_login_userInfo", mypage_ctrl.first_login_userInfo);
 //마이페이지 수정(기본 정보는 카카오 로그인할 때 담긴다)
 router.post("/profile_modify", mypage_ctrl.profile_modify);
+
+
 //마이페이지
 router.get("/show_me", mypage_ctrl.show_me);
 
